404dbbb1bfa4546e3d5e98dc65519fd3aa1db972
vpotnuru <vpotnuru@dmchosting.ca>
2022-06-20 23:31:18 -0400

    minor bugs resolved

--- crwn-cloathing/src/Routes/sign-in/sign-in.component.jsx
+++ crwn-cloathing/src/Routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 
 function SignIn(props) {
     const logGoogleUser = async () =>{
-        const response = await signInWithGooglePopup();
-        console.log(response);
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
     }
     return (
         <div>
